74912f23b253ec62abf677ef73c68b78c5b10d4e
himanshu208 <himanshu.208@gmail.com>
2017-04-25 14:49:25 +0530

    asdf
    asdfas

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var http = require('http');
-
-http.createServer(function (request, response) {
-    response.writeHead(200, {
-        'Content-Type': 'text/plain',
-        'Access-Control-Allow-Origin' : '*'
-    });
-    response.end('Hello World\n');
-}).listen(1337);
+var sys = require("sys"),
+my_http = require("http");
+my_http.createServer(function(request,response){
+  sys.puts("I got kicked");
+  response.writeHeader(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(8080);
+sys.puts("Server Running on 8080"); 
